0d89f4a169703c1d37c79297306a88cae5adcf94
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2021-03-21 16:21:45 +0600

    dot in regex

--- characterClass.js
+++ characterClass.js
@@ -1,3 +1,12 @@
-let regex = /abc/; // match extact characters in a string 
-let str = "abccca"
-console.log(str.match(regex)); // output: "abc"
+{
+  let regex = /abc/; // match extact characters in a string 
+  let str = "abccca"
+  console.log(str.match(regex)); // output: "abc"
+}
+
+{
+  let regex = /./; // matches first character except new line
+  let str = "You are Fool";
+  console.log(str.match(regex))  
+  //Output : ['Y']
+}
