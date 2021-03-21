8ba5337c095519640ab7baac14361007c4779119
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2021-03-21 16:28:27 +0600

    global Flag Usages

---
+++ flags.js
@@ -0,0 +1,14 @@
+// Global Flag
+// Retain the index of the last match, 
+//allowing subsequent searches to start from the end of the previous match.
+{
+  let regex = /./g; // matches every character except new line
+  let str = "You";
+  console.log(str.match(regex))  //Output: ['Y', 'o', 'u']
+}
+// Without Global Flag
+{
+  let regex = /./; // matches every character except new line
+  let str = "You";
+  console.log(str.match(regex))  //Output: [ 'Y', index: 0, input: 'You', groups: undefined ]
+}
