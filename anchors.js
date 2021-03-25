48fba43f38e7b5baf1af7f58b24a081ff6dffe2a
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2021-03-25 16:39:33 +0600

    word and not word boundary

--- anchors.js
+++ anchors.js
@@ -9,3 +9,19 @@
   let str = "she sells seashells"
   console.log(str.match(regex)); // output: "she"
 }
+
+{
+  let regex = /(\bs|s\b)/g; // Matches a word boundary position between a word character and non-word character or position (start / end of string).
+                           //   See the word character class (w) for more info.
+  let str = "she sells seashells"
+  console.log(str.match(regex)); // it matches all s in  befinning or end of a string
+  // \bs is beginning
+  // s\b is end
+  // | using as OR Operator
+}
+
+{
+  let regex = /sh\B/g; // Matches any position that is not a word boundary. This matches a position, not a character.
+  let str = "she sells seashells"
+  console.log(str.match(regex)); // it matches all sh in where is not boundery
+}
